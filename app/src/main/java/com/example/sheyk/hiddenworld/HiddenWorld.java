80b4f6993a4ebce8b6da9d0b75bd2888dadf554f
Sheyky <sheykyblaze@gmail.com>
2018-07-08 01:19:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sheyk/hiddenworld/HiddenWorld.java
@@ -0,0 +1,13 @@
+package com.example.sheyk.hiddenworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HiddenWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hidden_world);
+    }
+}
